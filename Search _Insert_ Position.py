b6e32abe36780873a8018d6a5f99538d37e713f5
syedaasfiyabanu <syedaasfiyabanubsc24@rvu.edu.in>
2026-01-30 11:03:43 +0530

    Add searchInsert method to Solution class
    Implement searchInsert method to find the insert position for a target in a sorted list.

---
+++ Search _Insert_ Position.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        for i in range(len(nums)):
+            if nums[i] >= target:
+                return i
+        # If target is greater than all elements, insert at the end
+        return len(nums)
